f5e662e13cdb9f182f5f10a03fb27a7aa51e8c3a
yaqeensh24-meet <yaqeen1202yq@gmail.com>
2024-07-14 23:11:07 +0300

    Update python_review.py

--- python_review.py
+++ python_review.py
@@ -1,15 +1,32 @@
-import random
+import random 
 
-temperatures = []
-
-for i in range(7):
-    temperature = random.uniform(26, 40)
-    temperatures.append(temperature)
-days=[onday, Tuesday, Wednesday, Thursday, Friday, Saturday,sunday]
-count=0
+temp=[]
 for i in range (7):
-	if (temperatures[i]%2==0):
-		count++
-		even_temperatures_days.append:(number[])
+	temp.append(random.randint(26,41))
+days_of_the_week=["sunday","monday","tuesday","wednsday","thirsday","friday","saturday"]
+
+good_days_count=0
+good_days=[]
+even_days=[]
+
+for i in range (len(temp)):
+	if temp[i]%2==0:
+		good_days_count+=1
+		even_days.append(days_of_the_week[i])
+
+max_temp=max(temp)
+min_temp=min(temp)
+max_temp_day=days_of_the_week[temp.index(max_temp)]
+min_temp_day=days_of_the_week[temp.index(min_temp)]
+
+avg_temp= sum(temp)/len(temp)
+above_avg=[temp for temp in temp if temp>avg_temp]
+
+print("the weather report of the week:" temp)
+print("shelly had", even_days , "good days")
+print("the hotest temp was:", max_tempt,"on", max_temp_day)
+print("the coldest temp was:", min_tempt,"on", min_temp_day) 
+print("days that were above the average temps:", above_avg)
+
 
 
