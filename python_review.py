36a216c7163901d7249a0deb039e25793bb685b9
yaqeensh24-meet <yaqeen1202yq@gmail.com>
2024-07-14 17:17:53 +0300

    Create python_review.py

---
+++ python_review.py
@@ -0,0 +1,15 @@
+import random
+
+temperatures = []
+
+for i in range(7):
+    temperature = random.uniform(26, 40)
+    temperatures.append(temperature)
+days=[onday, Tuesday, Wednesday, Thursday, Friday, Saturday,sunday]
+count=0
+for i in range (7):
+	if (temperatures[i]%2==0):
+		count++
+		even_temperatures_days.append:(number[])
+
+
